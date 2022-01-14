02d7ae3bca580ced3417d926ed947ef0fdc14ef5
francis-siva <fsivasithamparam@gmail.com>
2022-01-14 02:43:40 +0100

    Display offers with required elements in Joblist

--- src/components/job_offer/Job_offer.js
+++ src/components/job_offer/Job_offer.js
@@ -14,27 +14,27 @@ const JobOffer = (props) => {
             return (publishedDate)? `Crée le ${publishedDate}` : `N/A`
         }
     }
-    
+
     return (
         <div className='job_offer_container'>
-            <div>
-                <div>
+            <div className='offer_top_zone'>
+                <div className='offer_top_zone-left'>
                     <span><i className="fa-solid fa-star"></i></span> {props.jobCategory}
                 </div>
-                <div>{offerDate(jobUpdatedDate, jobCreatedDate)}</div>
-                <div><i className="fa-solid fa-heart"></i></div>
+                <div className='offer_top_zone-right'>{offerDate(jobUpdatedDate, jobCreatedDate)}</div>
+                <div className='heart'><i className="fa-solid fa-heart"></i></div>
             </div>
-            <div>{props.jobTitle}</div>
-            <div>
+            <div className='offer_middle_zone-upper'>{props.jobTitle}</div>
+            <div className='offer_middle_zone-lower'>
                 <div>{props.workPlace}</div>
                 <div>{props.contractType}</div>
             </div>
-            <div>
+            <div className='offer_bottom_zone'>
                 {props.society
-                    ? <div>{props.society}</div>
-                    : <div>Nom de l'entreprise non renseigné.</div>
+                    ? <div className='company'>{props.society}</div>
+                    : <div className='unknow_company'>Nom de l'entreprise non renseigné.</div>
                 }
-                <div><button>Postuler <span><i className="fa-solid fa-arrow-right"></i></span></button></div>
+                <div className='apply-area'><button>Postuler <span><i className="fa-solid fa-arrow-right"></i></span></button></div>
             </div>
         </div>
     );
